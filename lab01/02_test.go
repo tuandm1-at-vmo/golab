a12cfaa61c989ea218d1e5a216806316dccfc32f
Tuanm <gmail@tuanm.dev>
2023-07-23 10:04:10 +0700

    rename package lab to lab01

--- lab01/02_test.go
+++ lab01/02_test.go
@@ -1,4 +1,4 @@
-package lab
+package lab01
 
 import (
 	"reflect"
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 	"strings"
 
-	"tuanm.dev/golab/lab"
+	"tuanm.dev/golab/lab01"
 )
 
 var reader = bufio.NewReader(os.Stdin)
@@ -27,30 +27,30 @@ func main() {
 
 	switch i {
 	case 1:
-		runLab01()
+		runLab011()
 	case 2:
-		runLab02()
+		runLab012()
 	case 3:
-		runLab03()
+		runLab013()
 	default:
 		println("no lab found")
 	}
 }
 
-func runLab01() {
+func runLab011() {
 	print("tell me something: ")
 	text, _ := reader.ReadString('\n')
 	println("you can count on me like:")
-	for word, count := range lab.CountWords(text) {
+	for word, count := range lab01.CountWords(text) {
 		fmt.Printf("- %s: %d\n", word, count)
 	}
 }
 
-func runLab02() {
+func runLab012() {
 	print("tell me a number: ")
 	var n uint
 	fmt.Scan(&n)
-	primes, err := lab.GetAllPrimesUpTo(n, "./lab/first-1000000-primes.json")
+	primes, err := lab01.GetAllPrimesUpTo(n, "./lab/first-1000000-primes.json")
 	if err != nil {
 		println(err.Error())
 	} else {
@@ -66,7 +66,7 @@ func runLab02() {
 	}
 }
 
-func runLab03() {
+func runLab013() {
 	print("please enter a url: ")
 	url, _ := reader.ReadString('\n')
 	url = strings.Replace(url, "\n", "", -1) // remove last newline
@@ -74,7 +74,7 @@ func runLab03() {
 	if !hasProtocol {
 		url = "https://" + url
 	}
-	links, err := lab.CrawlAnchorsFrom(url)
+	links, err := lab01.CrawlAnchorsFrom(url)
 	if err != nil {
 		println(err.Error())
 	} else {
